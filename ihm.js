f4bfb2e8efdc6e104df83f7d15bd63245457cccf
Mehdi.Zahour <zahour.mehdi@gmail.com>
2018-05-07 10:29:32 +0200

    Modification du tp avec les normes EMCAScript2015

--- ihm.js
+++ ihm.js
@@ -1,46 +1,56 @@
-var service = require('./service');
-var request = require('request');
-var readline = require('readline');
+const serv = require('./service');
+const request = require('request-promise-native');
+const readline = require('readline');
 
-var rl = readline.createInterface({
+const service = new serv.service();
+const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
-var menu = function () {
-    rl.question("1. Rafraichir les données \n2. Lister les sessions \n99. Quitter \n", function (saisie) {
 
-        switch (saisie) {
-            case "1":
 
-                service.init(function (callback) {
-                    console.log(callback," : données mises à jours .....\n");
-                })
-
-                break;
-            case "2":
-                service.listerSessions(function (talk) {
-                    talk.forEach(element =>
-                        console.log(element.name, " (", element.speakers, ")")
-                    )
-                });
-                break;
-
-            case "99":
-                rl.close();
-                break;
-
-            default:
-                console.log("L'option que vous avez choisis est incorrect \n");
-                break;
-        }
-        if(saisie!=99) menu();
-    })
-
+const menu = () => {
+    rl.question(`
+    1. Rafraichir les données
+    2. Lister les sessions
+    3. Lister les présentateurs
+    99. Quitter
+    `, function (saisie) {
+            switch (saisie) {
+                case "1":
+                    service.init().then(nb=> {
+                        console.log(`${nb} : données mises à jours .....`);
+                        menu()
+                    })
+                    break;
+                case "2":
+                    service.listerSessions().then(talk => {
+                        talk.forEach(element =>
+                            console.log(`${element.name} ( ${element.speakers} )`)
+                        )
+                        menu()
+                    });
+                    break;
+                case "3":
+                    service.listerPresentateurs().then( presentateurs =>{
+                        presentateurs.forEach(element => console.log(element))
+                        menu()
+                    });
+                    break;
+                case "99":
+                    rl.close();
+                    break;
+                default:
+                    console.log(`L'option que vous avez choisis est incorrect `);
+                    menu()
+                    break;
+            }
+        })
 }
 
-exports.start = function () {
-    service.init(function (nb) {
-        console.log('[init]')
-        menu();
-    });
+exports.start = () => {
+    service.init().then(nb=> {
+        console.log(`${nb} : données mises à jours .....`);
+        menu()
+    })
 };
--- service.js
+++ service.js
@@ -1,25 +1,56 @@
-var request = require('request')
+const rp = require('request-promise-native');
+const jsdom = require('jsdom');
+
+
 
 // tableau qui contiendra toutes les sessions du BreizhCamp
-var talks = [];
-
-exports.init = function (callback) {
-    talks = [];
-    request('http://www.breizhcamp.org/json/talks.json', { json: true }, function(err, res, body) {
-        if (err) { return console.log('Erreur', err); }
-        talks = talks.concat(body);
-        // body contient les données récupérées
-
-
-        request('http://www.breizhcamp.org/json/others.json', { json: true }, function(err, res, body) {
-            if (err) { return console.log('Erreur', err); }
-            talks = talks.concat(body);
-            // body contient les données récupérées
-            callback(talks.length);
-        });
-    });
+let talks = [];
+let pres = [];
+let options = {
+    method: 'GET',
+    uri: 'http://www.breizhcamp.org/json/talks.json',
+    json: true
 };
 
-exports.listerSessions = function(callback){
-    callback(talks);
+exports.service = class Service {
+    init () {
+        talks = [];
+        options.uri = 'http://www.breizhcamp.org/json/talks.json';
+        return rp(options)
+            .then(body => {
+                talks = talks.concat(body);
+                options.uri = 'http://www.breizhcamp.org/json/others.json';
+                return rp(options)
+                    .then(b => {
+                        talks = talks.concat(b);
+                        return talks.length
+                    })
+                    .catch(err => console.log('Erreur', err))
+            }).catch(err => console.log('Erreur', err))
+    }
+
+    listerPresentateurs () {
+        pres = [];
+        options.uri = 'http://www.breizhcamp.org/conference/speakers/'
+        return rp(options).then(body => {
+            const dom = new jsdom.JSDOM(body);
+            const langs = dom.window.document.querySelectorAll("h3");
+            langs.forEach((lg) => {
+                pres.push(lg.innerHTML);
+            })
+            return pres;
+        }).catch(err => console.log('Erreur', err))
+    }
+
+    listerSessions () {
+        return new Promise((resolve, reject) => {
+            if (talks)
+                resolve(talks)
+            else
+                reject(`Erreur`)
+        })
+    }
+
 }
+
+
