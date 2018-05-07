4ec94ceeb24c238a7111ad8c1520b36d9625fe1e
cyril.quere <cyril.quere@gmail.com>
2018-05-07 10:29:29 +0200

    01d - lister presentateur + TP ECMAScript 2015

--- ihm.js
+++ ihm.js
@@ -1,33 +1,50 @@
-var service = require("./service");
-var readline = require("readline");
+const Service = require("./service");
+const readline = require("readline");
 
-var rl = readline.createInterface({
+const service = new Service.class()
+
+
+const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
-exports.start = function() {
-  service.init(function(nb) {
-    console.log("[init]", nb, "sessions trouvées.");
+exports.start = () => {
+  service.init().then(nb => {
+    console.log(`[init] ${nb} sessions trouvées.`);
     menu();
   });
 };
 
 function menu() {
   rl.question(
-    "*************************\n  1. Rafraichir les données\n  2. Lister les sessions\n 99. Quitter\n Veuillez choisir une option :",
-    function(saisie) {
+    `*************************
+      1. Rafraichir les données
+      2. Lister les sessions
+      3. Lister les présentateurs
+      99. Quitter
+      Veuillez choisir une option :`,
+    saisie => {
       switch (saisie) {
         case "1":
-          service.init(function(nb) {
-            console.log(nb, "Données mises à jour");
+          service.init().then(nb => {
+            console.log(`${nb} Données mises à jour`);
             menu();
           });
           break;
         case "2":
-          service.listerSessions(function(talks) {
+          service.listerSessions().then(talks => {
             talks.forEach(elem =>
-              console.log(elem.name, "(", elem.speakers, ")")
+              console.log(`${elem.name} (${elem.speakers})`)
+            )
+            
+            menu();
+          }).catch(err=> console.log(err));
+          break;
+          case "3":
+          service.listerPresentateurs().then(presentateurs => {
+            presentateurs.forEach(elem =>
+              console.log(elem)
             );
             menu();
           });
--- service.js
+++ service.js
@@ -1,38 +1,56 @@
+const jsdom = require("jsdom");
+const request = require("request-promise-native");
+
 // tableau qui contiendra toutes les sessions du BreizhCamp
-var talks = [];
-
-exports.init = function(callback) {
-  var request = require("request");
-  talks = []
-  request(
-    "http://www.breizhcamp.org/json/others.json",
-    { json: true },
-    function(err, res, body) {
-      if (err) {
-        return console.log("Erreur", err);
-      }
-      talks = talks.concat(body);
-      // body contient les données récupérées
-      //console.log("Ok", talks);
-
-      request(
-        "http://www.breizhcamp.org/json/talks.json",
-        { json: true },
-        function(err, res, body) {
-          if (err) {
-            return console.log("Erreur", err);
-          }
-          talks = talks.concat(body);
-
-          // body contient les données récupérées
-          //console.log("Ok", talks);
-          callback(talks.length);
-        }
-      );
-    }
-  );
+let talks = [];
+let presentateurs = [];
+let options = {
+  method: "GET",
+  uri: "",
+  json: true
 };
 
-exports.listerSessions = function(callback){
-    callback(talks)
-};
+exports.class =   class Service {
+  
+  init(){
+    talks = [];
+    options.uri = "http://www.breizhcamp.org/json/others.json";
+    return request(options)
+      .then(body => {
+        talks = talks.concat(body);
+        options.uri = "http://www.breizhcamp.org/json/talks.json";
+        return request(options)
+          .then(bdy => {
+            talks = talks.concat(bdy);
+            return talks.length;
+          })
+          .catch(err => console.log(`Erreur ${err}`));
+      })
+      .catch(err => console.log(`Erreur ${err}`));
+  }
+
+  listerSessions() {
+    return new Promise((resolve, reject) => {
+      if (talks)
+      resolve(talks)
+      else
+      reject(`La liste des sessions n'est pas chargé`)
+    })
+  }
+
+  listerPresentateurs  ()  {
+    options.uri = "http://www.breizhcamp.org/conference/speakers/";
+    presentateurs = [];
+    return request(options)
+      .then(body => {
+        const dom = new jsdom.JSDOM(body);
+        const langs = dom.window.document.querySelectorAll("h3");
+        langs.forEach(function(lg) {
+          presentateurs.push(lg.innerHTML);
+        });
+        return presentateurs;
+      })
+      .catch(err => console.log(`Erreur ${err}`));
+  }
+}
+
