bbec30b8d9662b9883d3a00c631c0fe50d16ddbe
Fabien SCOLAN <fabien.scolan22@gmail.com>
2018-05-07 17:36:16 +0200

    correction pb import + ajout protos

--- service.js
+++ service.js
@@ -1,4 +1,4 @@
-import { request } from "https";
+//import { request } from "https";
 
 // tableau qui contiendra toutes les sessions du BreizhCamp
 /*var talks = [];
@@ -13,14 +13,15 @@ var talks = [];
 exports.init = function (callback) {
 
     // TODO effectuer les requêtes HTTP permettant de récupérer les données du BreizhCamp
-    const req = new XMLHttpRequest();
+    //const req = new XMLHttpRequest();
     var request = require('request')
     request('http://www.breizhcamp.org/json/talks.json', { json: true }, function(err, res, body) {
-        if (err) { return console.log('Erreur', err); } });
-    talks = body;
+        if (err) { return console.log('Erreur', err); }
+        talks = body; });
+    
 
     // TODO     => une fois les données récupérées, alimenter la variable talks
 
     // TODO         => invoquer la callback avec le nombre de sessions récupérées
-    callback(talks.length());
+    callback(talks.length);
 };
